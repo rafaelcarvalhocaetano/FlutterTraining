74dc5d3f902e5219278890af45107bd83ebce4ec
Rafael Carvalho <rapha.pse@outlook.com>
2020-02-25 14:08:35 -0300

    Add refactor service

--- bytebank_api/lib/http/service.dart
+++ bytebank_api/lib/http/service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:bytebank_api/http/client.dart';
-import 'package:bytebank_api/models/contact.dart';
 import 'package:bytebank_api/models/transaction.dart';
 import 'package:http/http.dart';
 
@@ -12,30 +11,29 @@ class Service {
     final List<Transaction> transactions = List();
 
     for(Map<String, dynamic> element in decodedJson) {
-      final Map<String, dynamic> contactJson = element['contact'];
-
-      final Transaction trt = Transaction(
-          element['value'],
-          Contact(
-            0,
-            contactJson['name'],
-            contactJson['accountNumber'],
-          )
-      );
-      transactions.add(trt);
+//      final Map<String, dynamic> contactJson = element['contact'];
+//      final Transaction trt = Transaction(
+//          element['value'],
+//          Contact(
+//            0,
+//            contactJson['name'],
+//            contactJson['accountNumber'],
+//          )
+//      );
+      transactions.add(Transaction.fromJson(element));
     }
     return transactions;
   }
 
   Future<Transaction> save(Transaction transaction) async {
-    final Map<String, dynamic> transactionMap = {
-      'value': transaction.value,
-      'contact': {
-        'name': transaction.contact.name,
-        'accountNumber': transaction.contact.accountNumber
-      }
-    };
-    final String transactionJSON = jsonEncode(transactionMap);
+//    final Map<String, dynamic> transactionMap = {
+//      'value': transaction.value,
+//      'contact': {
+//        'name': transaction.contact.name,
+//        'accountNumber': transaction.contact.accountNumber
+//      }
+//    };
+    final String transactionJSON = jsonEncode(transaction.toJson());
 
     final Response request = await client.post(
         baseUrl,
@@ -45,17 +43,17 @@ class Service {
         },
         body: transactionJSON
     );
-
     final Map<String, dynamic> json = jsonDecode(request.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-
-    return Transaction(
-      json['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
+    return Transaction.fromJson(json);
+//    final Map<String, dynamic> contactJson = json['contact'];
+//
+//    return Transaction(
+//      json['value'],
+//      Contact(
+//        0,
+//        contactJson['name'],
+//        contactJson['accountNumber'],
+//      ),
+//    );
   }
 }
